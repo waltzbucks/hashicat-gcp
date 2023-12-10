a585edfc4aaf2375cf7a455d2d4104b1419ca9bd
hedy <waltzbox@gmail.com>
2023-12-10 15:18:55 +0000

    Added vpc module

--- vpc.tf
+++ vpc.tf
@@ -13,25 +13,4 @@ module "vpc" {
             subnet_region = var.region
         }
     ]
-
-    secondary_ranges = {
-        subnet-01 = [
-            {
-                range_name    = "subnet-01-secondary-01"
-                ip_cidr_range = "192.168.64.0/24"
-            },
-        ]
-
-        subnet-02 = []
-    }
-
-    routes = [
-        {
-            name                   = "egress-internet"
-            description            = "route through IGW to access internet"
-            destination_range      = "0.0.0.0/0"
-            tags                   = "egress-inet"
-            next_hop_internet      = "true"
-        },
-    ]
 }
