a611a7ddcbbcd1e68d71d5c9fe1bd5106761e7ca
Your Name <you@example.com>
2023-12-11 14:01:25 +0000

    Added vpc module

--- vpc.tf
+++ vpc.tf
@@ -1,29 +1,4 @@
 module "vpc" {
-  source  = "app.terraform.io/ACME-TF-WORKSHOP/network/google"
-  version = "3.4.0"
-
-    project_id   = var.project
-    network_name = "gaurav-network"
-    routing_mode = "GLOBAL"
-
-    subnets = [
-        {
-            subnet_name   = "gaurav-subnet"
-            subnet_ip     = "10.100.10.0/24"
-            subnet_region = var.region
-        }
-    ]
-    routes = [
-        {
-            name                   = "egress-internet"
-            description            = "route through IGW to access internet"
-            destination_range      = "0.0.0.0/0"
-            tags                   = "egress-inet"
-            next_hop_internet      = "true"
-        },
-    ]
-}
-module "vpc" {
     source  = "terraform-google-modules/network/google"
     version = "~> 8.1"
 
